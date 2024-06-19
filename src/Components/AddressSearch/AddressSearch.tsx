52a163f4262a0ba85dc38502237b664e5888a278
Steve Giordano <steve1nyc@gmail.com>
2024-06-19 14:31:55 -0400

    Use dropdown style

--- src/Components/AddressSearch/AddressSearch.tsx
+++ src/Components/AddressSearch/AddressSearch.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import Select, { InputActionMeta, OnChangeValue, Props } from "react-select";
+import Select, { OnChangeValue, Props } from "react-select";
 
 export type SelectOption = {
   value: string;
@@ -7,48 +6,19 @@ export type SelectOption = {
 };
 interface AddressSearchProps extends Props {
   options: SelectOption[];
-  onSelection: (newValue: string | null) => void;
+  onSelection: (newValue: OnChangeValue<SelectOption, false>) => void;
 }
 
 export const AddressSearch: React.FC<AddressSearchProps> = ({
   onSelection,
   options,
 }) => {
-  const [openMenu, setOpenMenu] = useState(false);
-
-  const handleOnChange = (newValue: OnChangeValue<SelectOption, false>) => {
-    hideMenu();
-    if (onSelection) {
-      onSelection(newValue ? newValue.value : null);
-    }
-  };
-
-  const handleInputChange: (
-    newValue: string,
-    actionMeta: InputActionMeta,
-  ) => void = (_query, { action }) => {
-    if (action === "input-change") {
-      setOpenMenu(true);
-    }
-  };
-
-  const hideMenu = () => {
-    setOpenMenu(false);
-  };
-
   return (
     <Select
       options={options}
       placeholder={"Select address..."}
       noOptionsMessage={() => "No match"}
-      onChange={handleOnChange}
-      menuIsOpen={openMenu}
-      onInputChange={handleInputChange}
-      onBlur={hideMenu}
-      components={{
-        IndicatorSeparator: () => null,
-        DropdownIndicator: () => null,
-      }}
+      onChange={onSelection}
     />
   );
 };
--- src/Components/Pages/Buildings/NoBBL.tsx
+++ src/Components/Pages/Buildings/NoBBL.tsx
@@ -1,15 +1,16 @@
 // import { AddressRecord } from "../../types/APIDataTypes";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./style.scss";
-import { AddressSearch } from "../../AddressSearch/AddressSearch";
+import { AddressSearch, SelectOption } from "../../AddressSearch/AddressSearch";
 import selectOptions from "./buildings_options.json";
+import "./style.scss";
 
 export const NoBBL: React.FC = () => {
   const navigate = useNavigate();
-  const onSelection = (newValue: string | null) => {
+
+  const onSelection = (newValue: SelectOption | null) => {
     if (newValue) {
-      navigate(`/buildings?bbl=${newValue}`);
+      navigate(`/buildings?bbl=${newValue.value}`);
     }
   };
 
