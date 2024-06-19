4c94ee3ffd8cd7a8e8ebed2e28926036731f9081
Steve Giordano <steve1nyc@gmail.com>
2024-06-19 13:51:55 -0400

    Go to building on selection

--- src/Components/Pages/Buildings/NoBBL.tsx
+++ src/Components/Pages/Buildings/NoBBL.tsx
@@ -1,65 +1,26 @@
 // import { AddressRecord } from "../../types/APIDataTypes";
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useGetAllBBLs } from "../../../api/hooks";
-// import { BuildingHPDCompEmerg } from "../BuildingHPDCompEmerg/BuildingHPDCompEmerg";
-import { Button } from "@justfixnyc/component-library";
 import "./style.scss";
-import GeoAddressSearch, {
-  SearchAddress,
-} from "../../AddressSearch/GeoAddressSearch";
 import { AddressSearch } from "../../AddressSearch/AddressSearch";
 import selectOptions from "./buildings_options.json";
 
 export const NoBBL: React.FC = () => {
   const navigate = useNavigate();
-  const [selectedBBL, setSelectedBBL] = useState<string>();
-
-  const { data, isLoading, error } = useGetAllBBLs();
-
-  const onFormSubmit: (
-    searchAddress: SearchAddress,
-    error: unknown
-  ) => void = ({ bbl }) => {
-    navigate(`/buildings?bbl=${bbl}`);
-  };
   const onSelection = (newValue: string | null) => {
-    setSelectedBBL(newValue ?? "");
+    if (newValue) {
+      navigate(`/buildings?bbl=${newValue}`);
+    }
   };
 
-  const onSubmit = () => {
-    navigate(`/buildings?bbl=${selectedBBL}`);
-  };
 
   return (
     <>
       <h2>Buildings</h2>
-      Search for a building by address (Pre-filtered local list)
+      Search for a building by address
       <br />
       <br />
       <AddressSearch options={selectOptions} onSelection={onSelection} />
-      <br />
-      <Button labelText="Search Building" size="small" onClick={onSubmit} />
-      <br />
-      <br />
-      <br />
-      Search for a building by address (using the city's api and filtering
-      results)
-      <br />
-      <br />
-      {isLoading && <div>loading...</div>}
-      {error && <pre>{JSON.stringify(error, null, 2)}</pre>}
-      {data && (
-        <>
-          <GeoAddressSearch
-            onFormSubmit={onFormSubmit}
-            labelText=""
-            labelClass={""}
-            bbls={data}
-          />
-          <Button labelText="Search Building" size="small" />
-        </>
-      )}
     </>
   );
 };
