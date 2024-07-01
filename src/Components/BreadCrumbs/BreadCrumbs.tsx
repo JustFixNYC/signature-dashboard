db7b7dbdbfc7f7ebff0e7df83f7fe7b52b7be707
Steve Giordano <steve1nyc@gmail.com>
2024-07-01 14:14:53 -0400

    Style breadcrumb links like JFCL links

--- src/Components/BreadCrumbs/BreadCrumbs.tsx
+++ src/Components/BreadCrumbs/BreadCrumbs.tsx
@@ -9,7 +9,7 @@ export const BreadCrumbs: React.FC<BreadCrumbs> = ({ crumbs }) => {
   const crumbpath = crumbs.flatMap((crumb, index, array) => {
     if (index !== array.length - 1) {
       return [
-        <Link key={index} to={crumb.path ?? "/"}>{crumb.name}</Link>,
+        <Link key={index} to={crumb.path ?? "/"} className="jfcl-link">{crumb.name}</Link>,
         <span key={index + "caret"} className="breadCrumbs__caret">
           <Icon icon="caretRight" className="" />
         </span>,
