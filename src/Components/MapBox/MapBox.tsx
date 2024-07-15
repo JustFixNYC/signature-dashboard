92c304e530dd6349f1620efc92753c0fac3812a1
Maxwell Austensen <m.austensen@gmail.com>
2024-07-15 16:50:02 -0400

    use JFCL X icon

--- src/Components/MapBox/MapBox.tsx
+++ src/Components/MapBox/MapBox.tsx
@@ -6,7 +6,7 @@ import React, { useState } from "react";
 import { MapData } from "../../types/APIDataTypes";
 import mapboxgl from "mapbox-gl";
 import { Link } from "react-router-dom";
-// import { Icon } from "@justfixnyc/component-library";
+import { Icon } from "@justfixnyc/component-library";
 
 const STYLE_SIGNATURE_LIGHT =
   "mapbox://styles/justfix/clxummt2k047a01qj3ra1gjf6";
@@ -171,7 +171,7 @@ export const MapBox: React.FC<MapBoxProps> = ({ data, initialSelectedBBL }) => {
                 {`${selectedAddr.address}, ${selectedAddr.borough.toUpperCase()}`}
               </Link>
               <button onClick={() => setSelectedAddr(null)}>
-                ✕{/* <Icon icon="xmark" /> */}
+                <Icon icon="xmark" />
               </button>
             </div>
 
