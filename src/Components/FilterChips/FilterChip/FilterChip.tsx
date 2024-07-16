bc242bb38cc524ef9cd7ccf3bbcc37b4e136478a
Steve Giordano <steve1nyc@gmail.com>
2024-07-16 16:19:33 -0400

    WIP

--- src/Components/FilterChips/FilterChip/FilterChip.tsx
+++ src/Components/FilterChips/FilterChip/FilterChip.tsx
@@ -1,6 +1,7 @@
 import { Icon } from "@justfixnyc/component-library";
 import classNames from "classnames";
 import "./style.scss";
+import React, { LegacyRef } from "react";
 
 type FilterChipProps = {
   labelText: string;
@@ -9,35 +10,35 @@ type FilterChipProps = {
   variant?: "primary" | "secondary"; // To be used in the future when migrated to component library
   className?: string;
   onClick?: () => void;
+  ref: LegacyRef<HTMLDivElement> | undefined;
 };
 
-export const FilterChip: React.FC<FilterChipProps> = ({
-  labelText,
-  removable,
-  selected,
-  variant = "primary",
-  onClick,
-  className,
-}) => {
-  const filterChipClassNames = classNames(
-    "jfcl-filter-chip",
-    `jfcl-variant-${variant}`,
-    {
-      ["jfcl-filter-chip--is-selected"]: selected,
-    },
-    className
-  );
-  return (
-    <div
-      className={filterChipClassNames}
-      {...(onClick && { onClick: onClick })}
-    >
-      <span className="jfcl-filter-chip__text">{labelText}</span>
-      {removable && (
-        <span className="jfcl-filter-chip__icon_remove">
-          <Icon icon="xmark" />
-        </span>
-      )}
-    </div>
-  );
-};
+export const FilterChip = React.forwardRef<HTMLDivElement, FilterChipProps>(
+  (
+    { labelText, removable, selected, variant = "primary", onClick, className },
+    ref
+  ) => {
+    const filterChipClassNames = classNames(
+      "jfcl-filter-chip",
+      `jfcl-variant-${variant}`,
+      {
+        ["jfcl-filter-chip--is-selected"]: selected,
+      },
+      className
+    );
+    return (
+      <div
+        className={filterChipClassNames}
+        {...(onClick && { onClick: onClick })}
+        ref={ref}
+      >
+        <span className="jfcl-filter-chip__text">{labelText}</span>
+        {removable && (
+          <span className="jfcl-filter-chip__icon_remove">
+            <Icon icon="xmark" />
+          </span>
+        )}
+      </div>
+    );
+  }
+);
--- src/Components/FilterChips/FilterChips.tsx
+++ src/Components/FilterChips/FilterChips.tsx
@@ -2,7 +2,7 @@ import { Button } from "@justfixnyc/component-library";
 import { apiKeys, getColumnHeader } from "../../util/helpers";
 import { FilterChip } from "./FilterChip/FilterChip";
 import { ColumnFiltersState } from "@tanstack/react-table";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 type FilterChipsProps = {
   columnFilters: ColumnFiltersState;
@@ -15,22 +15,48 @@ export const FilterChips: React.FC<FilterChipsProps> = ({
   removeFn,
 }) => {
   const [showAll, setShowAll] = useState(false);
+  const itemsRef = useRef<(HTMLDivElement | null)[]>([]);
 
-  const filtersToShow = showAll
-    ? columnFilters
-    : columnFilters.slice(0, defaultFilterCount);
+  // const filtersToShow = showAll
+  //   ? columnFilters
+  //   : columnFilters.slice(0, defaultFilterCount);
 
-  const filterChipsToShow = filtersToShow.map((filter) => {
+  const filterChipsToShow = columnFilters.map((filter, i) => {
     return (
       <FilterChip
         key={filter.id}
         labelText={getColumnHeader(filter.id as apiKeys)}
         selected={true}
         removable={true}
+        ref={ref => itemsRef.current[i] = ref }
         onClick={() => removeFn(filter.id)}
       />
     );
   });
+  // if (itemsRef.current[0]) {
+  //   console.log('~~', itemsRef.current[0].getBoundingClientRect().top)
+  // }
+  const tops = {};
+
+  itemsRef.current.forEach((el, i) => {
+    const top = el?.getBoundingClientRect().top;
+    if (top) {
+      const count = Object.keys(tops).length;
+      console.log('~~~', {count, exists: tops[top]})
+      if (!tops[top] && count >= 2) {
+        setShowAll(true);
+      } else {
+        tops[top] = true;
+      }
+    }
+
+    // console.log('~~', el?.getBoundingClientRect().top)
+})
+
+  // const totalWidth = itemsRef.current.reduce((total, curr) => {
+  //   if (!curr) return total;
+  //   return total += curr.offsetWidth + parseInt(curr.style.marginRight);
+  // }, 0)
   return (
     <>
       {filterChipsToShow}
