55f51a780fc4219a9ec6cfdfc511bbd62ed4ddff
Steve Giordano <steve1nyc@gmail.com>
2024-07-18 18:30:26 -0400

    Fix landlords table

---
+++ src/Components/Pill/BIPPill.tsx
@@ -0,0 +1,15 @@
+import { Pill } from "./Pill";
+import { formatNumber } from "../../util/helpers";
+
+type BIPPillProps = {
+  value: number;
+};
+
+export const BIPPill: React.FC<BIPPillProps> = ({ value }) => {
+  const color = value < 500 ? "grey" : value < 800 ? "yellow" : "orange";
+  return (
+    <Pill color={color}>
+      <>{formatNumber(value)}</>
+    </Pill>
+  );
+};
